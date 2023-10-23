53accb9a754c6b4d6790f95db26a353391df605a
TaceStewart <tacec.97@gmail.com>
2023-10-23 13:25:19 +1000

    Finished making sensitivity figures

--- Stewart_MPhil_DH_Sensitivity.R
+++ Stewart_MPhil_DH_Sensitivity.R
@@ -79,7 +79,6 @@ sector_boundaries <- st_read(shapefile_path,
 ############## SET VARIABLES ###############
 # Variables
 base_recov_th <- 0.75
-list_recov_th <- seq(0.05, 1, by = 0.05)
 list_recov_yrs <- seq(1, 15, by = 1)
 infer_baseline <- 1 # Should the baseline be inferred if non-existent at start of obs period?
 epsilon <- 0.05
@@ -181,7 +180,8 @@ mu <- reef_df_all %>%
   group_by(baseline_infer_bool) %>% 
   summarise(grp.mean = mean(initial_bl, na.rm = TRUE))
 
-ggplot(data = reef_df_all, 
+# Part A: Overall
+dc_sens_1a <- ggplot(data = reef_df_all, 
        aes(x = initial_bl)) +
   geom_density(aes(y = after_stat(density),
                fill = factor(baseline_infer_bool,
@@ -198,23 +198,70 @@ ggplot(data = reef_df_all,
   labs(x = "Initial Baseline Coral Cover (%)", 
        y = "Density",
        fill = "Baseline Inferred?",
-       color = "Baseline Inferred?") +
+       color = "Baseline Inferred?",
+       tag = "A") +
   theme_classic() +
   theme(axis.title = element_text(size = 12),
         axis.text = element_text(size = 10),
         legend.title = element_text(size = 12),
-        legend.text = element_text(size = 10),
-        legend.position = c(1, 1),
-        legend.justification = c(1, 1)) +
+        legend.text = element_text(size = 10)) +
   scale_fill_manual(values = c("0" = "#F8766D", "1" = "#00BFC4"),
                     labels = c("0" = "No", "1" = "Yes")) +
   scale_color_manual(values = c("0" = "#F8766D", "1" = "#00BFC4"),
                     labels = c("0" = "No", "1" = "Yes"))
 
+# Part B: By management area
+mu_1b <- reef_df_all %>%
+  group_by(baseline_infer_bool, sector) %>% 
+  summarise(grp.mean = mean(initial_bl, na.rm = TRUE))
+dc_sens_1b <- ggplot(data = reef_df_all, 
+    aes(x = initial_bl)) +
+  geom_density(aes(y = after_stat(density),
+      fill = factor(baseline_infer_bool,
+              levels = c(1, 0))),
+      alpha = 0.4,
+      color = "black",
+      linewidth = 1) +
+  geom_vline(aes(xintercept = grp.mean,
+       color = factor(baseline_infer_bool,
+              levels = c(1, 0))), 
+       data = mu_1b,
+       linetype = "dashed", 
+       linewidth = 1) +
+  labs(x = "Initial Baseline Coral Cover (%)", 
+    y = "Density",
+    fill = "Baseline Inferred?",
+    color = "Baseline Inferred?",
+    tag = "B") +
+  theme_classic() +
+  theme(axis.title = element_text(size = 12),
+     axis.text = element_text(size = 10),
+     legend.title = element_text(size = 12),
+     legend.text = element_text(size = 10)) +
+  scale_fill_manual(values = c("0" = "#F8766D", "1" = "#00BFC4"),
+        labels = c("0" = "No", "1" = "Yes")) +
+  scale_color_manual(values = c("0" = "#F8766D", "1" = "#00BFC4"),
+        labels = c("0" = "No", "1" = "Yes")) +
+  facet_wrap(~factor(sector, 
+                     levels = c("Far Northern Management Area",
+                                "Cairns/Cooktown Management Area",
+                                "Townsville/Whitsunday Management Area",
+                                "Mackay/Capricorn Management Area"),
+                     labels = c("Far Northern",
+                                "Cairns/Cooktown",
+                                "Townsville/Whitsunday",
+                                "Mackay/Capricorn")), 
+                     ncol = 1)
+
+# Combine the plots
+dc_sens_1 <- ggarrange(dc_sens_1a, dc_sens_1b, 
+                       ncol = 2, common.legend = TRUE, 
+                       legend = "bottom")
+
 # Save the plot
 ggsave(
   paste0(out_path, "/Sens1.png"),
-  width = 6, height = 5
+  width = 9, height = 6
 )
 ############################################
 
@@ -366,44 +413,149 @@ reef_df_all_sens3$num_disturbances <- as.numeric(reef_df_all_sens3$num_disturban
 dc_sens_3_df <- reef_df_all_sens3 %>%
   group_by(epsilon_val, dist_type) %>%
   summarise(mean = mean(num_disturbances, na.rm = TRUE),
-            sd = sd(num_disturbances, na.rm = TRUE),
-            n = n()) %>%
+      sd = sd(num_disturbances, na.rm = TRUE),
+      n = n()) %>%
   mutate(se = sd / sqrt(n),
-         lower = mean - (1.96 * se),
-         upper = mean + (1.96 * se))
+      lower = mean - (1.96 * se),
+      upper = mean + (1.96 * se),
+      sum_sc_means = sum(mean[dist_type != "num_total"], na.rm = TRUE),
+      sum_sc_sds = sum(sd[dist_type != "num_total"], na.rm = TRUE),
+      sum_sc_n = sum(n[dist_type != "num_total"], na.rm = TRUE),
+      sum_sc_se = sum(se[dist_type != "num_total"], na.rm = TRUE),
+      sum_sc_lower = sum(lower[dist_type != "num_total"], na.rm = TRUE),
+      sum_sc_upper = sum(upper[dist_type != "num_total"], na.rm = TRUE))
 
-ggplot(data = dc_sens_3_df) +
-  geom_point(size = 2, 
-       aes(x = epsilon_val, 
-           y = mean,
-           color = as.factor(dist_type))) +
+dc_sens_3a <- ggplot(data = dc_sens_3_df[dc_sens_3_df$dist_type != "num_total", ]) +
+  geom_point(size = 1, 
+    aes(x = epsilon_val, 
+     y = mean,
+     color = as.factor(dist_type))) +
   geom_errorbar(aes(x = epsilon_val,
-                    ymin = lower,
-                    ymax = upper,
-                    color = as.factor(dist_type)),
-                width = 0.01) +
+        ymin = lower,
+        ymax = upper,
+        color = as.factor(dist_type)),
+       width = 0.01) +
+  geom_point(size = 1, 
+    aes(x = epsilon_val, 
+     y = sum_sc_means,
+        color = "num_total_cat"),
+     color = "grey15") +
+  geom_errorbar(aes(x = epsilon_val,
+        ymin = sum_sc_lower,
+        ymax = sum_sc_upper,
+        color = "num_total_cat"),
+       width = 0.01) +
+  geom_hline(yintercept = mean(reef_df_all_sens3$num_disturbances[reef_df_all_sens3$dist_type == "num_total"], 
+                               na.rm = TRUE),
+       color = "grey30",
+       linetype = "dashed") +
+  geom_text(aes(x = 0.275,
+        y = mean(reef_df_all_sens3$num_disturbances[reef_df_all_sens3$dist_type == "num_total"], 
+                               na.rm = TRUE) + 0.025,
+        label = "Total Uncategorised Disturbances"),
+       color = "grey30",
+       size = 10 / .pt,
+       hjust = 0.5,
+       vjust = 0) +
   labs(x = "Epsilon Value", 
-       y = "Number of Disturbances",
-       color = "") +
+    y = "Number of Disturbances",
+    color = "Disturbance Type",
+    tag = "A") +
   theme_classic() +
   theme(axis.title = element_text(size = 12),
-        axis.text = element_text(size = 10),
-        legend.title = element_text(size = 12),
-        legend.text = element_text(size = 10)) +
-  scale_color_manual(values = c("num_total" = "grey", "num_single" = "steelblue1", "num_comp" = "steelblue4"),
-                    labels = c("num_total" = "Total", "num_single" = "Single", "num_comp" = "Cumulative"))
+     axis.text = element_text(size = 10),
+     legend.title = element_text(size = 12),
+     legend.text = element_text(size = 10)) +
+  scale_color_manual(values = c("num_total_cat" = "grey15", "num_single" = "steelblue1", "num_comp" = "steelblue4"),
+        labels = c("num_total_cat" = "Total Categorised", "num_single" = "Single", "num_comp" = "Cumulative"))
+
+# Get the assumed base case (epsilon = 0.05)
+base_case <- reef_df_all_sens3[reef_df_all_sens3$epsilon_val == 0.05, ] %>%
+  mutate(dist_type = factor(dist_type, levels = c("num_total", "num_single", "num_comp"))) %>%
+  filter(dist_type != "num_total")
+
+# Get all others
+others <- reef_df_all_sens3[reef_df_all_sens3$epsilon_val != 0.05, ] %>%
+  mutate(dist_type = factor(dist_type, levels = c("num_total", "num_single", "num_comp"))) %>%
+  filter(dist_type != "num_total")
+
+# Calculate number of reclassified disturbances from base case
+others <- others %>%
+  group_by(reef_name, dist_type, epsilon_val) %>%
+  summarise(num_disturbances = sum(num_disturbances, na.rm = TRUE))
+base_case <- base_case %>%
+  group_by(reef_name, dist_type) %>%
+  summarise(num_disturbances = sum(num_disturbances, na.rm = TRUE))
+others <- others %>%
+  left_join(base_case, by = c("reef_name", "dist_type")) %>%
+  mutate(num_reclassed = num_disturbances.x - num_disturbances.y) %>%
+  select(reef_name, dist_type, epsilon_val, num_reclassed)
+
+# Get a mean and confidence interval on the number of reclassified disturbances
+# for each epsilon value
+dc_sens_3_reclassed <- others %>%
+  group_by(epsilon_val, dist_type) %>%
+  summarise(mean = mean(num_reclassed, na.rm = TRUE),
+      sd = sd(num_reclassed, na.rm = TRUE),
+      n = n()) %>%
+  mutate(se = sd / sqrt(n),
+      lower = mean - (1.96 * se),
+      upper = mean + (1.96 * se))
+
+# Plot the number of reclassified disturbances from base case on top of dc_sens_3
+dc_sens_3b <- ggplot() +
+  geom_smooth(data = dc_sens_3_reclassed,
+    aes(x = epsilon_val,
+      y = mean,
+      color = as.factor(dist_type)),
+    method = "lm",
+    se = TRUE) +
+  geom_hline(yintercept = 0,
+     color = "grey30",
+     linetype = "dashed") +
+  labs(x = "Epsilon Value", 
+    y = "Average Reclassified",
+    color = "Disturbance Type",
+    tag = "B") +
+  theme_classic() +
+  theme(axis.title = element_text(size = 12),
+     axis.text = element_text(size = 10),
+     legend.title = element_text(size = 12),
+     legend.text = element_text(size = 10)) +
+  scale_color_manual(values = c("num_single" = "steelblue1", "num_comp" = "steelblue4"),
+        labels = c("num_single" = "Single", "num_comp" = "Cumulative"))
+
+# Combine the plots
+dc_sens_3 <- ggarrange(dc_sens_3a, dc_sens_3b, ncol = 1, nrow = 2, heights = c(0.6, 0.4))
+
 
 # Save the plot
 ggsave(
   paste0(out_path, "/Sens3.png"),
-  width = 6, height = 5
+  width = 6, height = 8
 )
 ############################################
 
 ########### SENSITIVITY 4 LOOP #############
-# Recovery Threshold
+# Sensitivity to Recovery Threshold
+epsilon <- 0.05
+list_recov_th <- seq(0, 1, by = 0.05)
+reef_df_all <- data.frame(matrix(NA, nrow = 0, ncol = 17))
 i <- 1
 for (recov_th in list_recov_th) {
+  all_reefs_sf <- all_reefs_sf %>%
+  mutate(
+    is_disturbed = (all_reefs_sf$COTS_value >= cots_dist |
+      all_reefs_sf$Hs4MW_value >= cyc_dist |
+      all_reefs_sf$DHW_value >= dhw_dist),
+    is_impacted = 0,
+    single_or_compound = NA,
+    dist_type = NA,
+    recov_year = NA,
+    recov_time = NA,
+    r_given_impact = NA
+  )
+
   # SINGLE OR COMPOUND DIST
   # Analyse reefs for single or compound disturbances
   # Returns event_counts, reef_df, overall_unknown_count
@@ -417,16 +569,62 @@ for (recov_th in list_recov_th) {
   # Extract the list elements
   event_counts <- sing_comp_list[[1]]
   all_reefs_sf <- sing_comp_list[[2]]
-  reef_df <- sing_comp_list[[3]]
+  reef_df <- sing_comp_list[[3]] %>%
+    mutate(recov_th_val = recov_th)
 
-  # Calculate probability of reef being in a recovered state
-  reef_df <- p_calculator(reef_df, mgmt_benefit)
-
+  reef_df_all[(nrow(reef_df_all) + 1):(nrow(reef_df_all) + nrow(reef_df)), 1:ncol(reef_df)] <- reef_df
   i <- i + 1
 }
+colnames(reef_df_all) <- colnames(reef_df)
+
 ############################################
 
 ########### DATA CHAPTER SENS 4 ############
-# Recovery threshold (update)
+# Violin plot of variation in probability of recovery with recovery threshold
+reef_df_all$recov_th_val <- as.numeric(reef_df_all$recov_th_val)
+reef_df_all$prob_s_recov <- as.numeric(reef_df_all$prob_s_recov)
+reef_df_all$prob_c_recov <- as.numeric(reef_df_all$prob_c_recov)
+
+# get a mean and confidence interval on the number of disturbances for each recovery_th value
+# and disturbance type
+dc_sens_4_df <- reef_df_all %>%
+  select(c(reef_name, recov_th_val, prob_s_recov, prob_c_recov)) %>%
+  melt(id.vars = c("reef_name", "recov_th_val"),
+       variable.name = "dist_type",
+       value.name = "prob_recov") %>%
+  group_by(recov_th_val, dist_type) %>%
+  summarise(mean = mean(as.numeric(prob_recov), na.rm = TRUE),
+      sd = sd(as.numeric(prob_recov), na.rm = TRUE),
+      n = n()) %>%
+  mutate(se = sd / sqrt(n),
+      lower = mean - (1.96 * se),
+      upper = mean + (1.96 * se))
+
+dc_sens_4 <- ggplot(data = dc_sens_4_df, 
+       aes(x = recov_th_val,
+            y = mean,
+            color = as.factor(dist_type))) +
+  geom_point(size = 1) +
+  geom_errorbar(aes(ymin = lower,
+                    ymax = upper),
+                width = 0.01) +
+  geom_smooth(method = "loess",
+              se = TRUE) +
+  labs(x = TeX("Recovery Threshold (Baseline \\times (1 - $\\rho$))"),
+        y = "Probability of Recovery",
+        color = "Disturbance Type") +
+  theme_classic() +
+  theme(axis.title = element_text(size = 12),
+        axis.text = element_text(size = 10),
+        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 10)) +
+  scale_color_manual(values = c("prob_s_recov" = "steelblue1", "prob_c_recov" = "steelblue4"),
+                    labels = c("prob_s_recov" = "Single", "prob_c_recov" = "Cumulative"))
+
+# Save the plot
+ggsave(
+  paste0(out_path, "/Sens4.png"),
+  width = 6, height = 5
+)
 
 ############################################
